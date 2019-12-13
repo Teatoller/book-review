994584bb2100c2a7933869ef444c6c97ebe8b312
Teatoller <steven.otieno1@gmail.com>
2019-12-13 20:00:38 +0300

    (controller): Implement rating controller
    - add rating controller
    - add method to store function
    
    [Starts #013]

---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use App\Http\Resources\RatingResource;
+use App\Rating;
+use Illuminate\Http\Request;
+
+class RatingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Book $id)
+    {
+        $rating = Rating::firstOrCreate(
+            [
+                'user_id' => $request->user()->id,
+                'book_id' => $id->id,
+            ],
+            ['rating' => $request->rating]
+        );
+
+        return new RatingResource($rating);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
